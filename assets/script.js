4492b97699213e06c3ae9140a51ee5db5e7de1f9
Mike Zanercik <Michael.zanercik@gmail.com>
2019-10-20 19:27:48 -0400

    added changes to js

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,82 @@
-
-        // var charAll = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&*+,-./:;<>=?@[]^_`\|/~"
-        // var passwordLength = prompt("How long would you like your password to be 'Please enter between 8 and 128'");
-        // var sChar = prompt("Please choose a special character");
-        // var nChar = prompt("Please choose a number");
-        // var lChar = prompt("Please choose a lowercase letter");
-        // var uChar = prompt("Please choose an uppercase letter");
-        // var yourPassword= " ";
-
-        // // var yourChar = [sChar, nChar, lChar, uChar]
-        // // console.log(yourChar);
-
-        // // console.log(password(passwordLength, yourChar, charAll));
-
-        // function password(){
-        //     for(var i = 0; i < passwordLength; i++){
-        //         password = password + charAll.charAt(Math.floor(Math.random()*Math.floor(charAll.length)));
-        //         }
-        //     }
-            
-        //  if (password.includes(sChar && nChar && lChar && uChar)){
-        // console.log(password);
-        //   }
-
-
-
-
-
-
-
-        // <!--first try-->
-
-        var passwordLength = prompt("How long would you like your password to be 'Please enter between 8 and 128'");
-        var sChar = prompt("Please choose a special character");
-        var nChar = prompt("Please choose a number");
-        var lChar = prompt("Please choose a lowercase letter");
-        var uChar = prompt("Please choose an uppercase letter");
-        var passwordValue = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&*+,-./:;<>=?@[]^_`\|/~"
-        var password= "";
-
-        // var passReq=[passwordLength, sChar, nChar, lChar, uChar];
-        // console.log(passReq);
-
-        for(var i = 0; i <= passwordLength; i++){
-        password = password + passwordValue.charAt(Math.floor(Math.random()*Math.floor(passwordValue.length)));
+var passwordDiv = document.getElementById("password");
+var generatePassBtn = document.getElementById("getPassword");
+generatePassBtn.addEventListener("click", handleClick);
+function handleClick(){
+    passwordDiv.innerText = generatePassword();
+}
+function getPasswordOptions() {
+    var length;
+    do {
+        length = parseInt(prompt("How long would you like your password to be?"));
+    } while (isNaN(length) || length < 8 || length > 128);
+    var sChar = confirm("Do you want special characters?");
+    var nChar = confirm("Do you want numbers?");
+    var lChar = confirm("Do you want lowercase characters?");
+    var uChar = confirm("Do you want uppercase characters?");
+    var options = {
+        length,
+        sChar,
+        nChar,
+        lChar,
+        uChar,
+    }
+    return options
+};
+function RandomNumber (bottom, top) {
+    var diff = (top - bottom) + 1;
+    return Math.floor(Math.random()* diff)+ bottom;
+}
+function RandomItemInArray(arr){
+    return arr[Math.floor(Math.random()*arr.length)]
+}
+function ShuffleArray(arr){
+    for(var i = 0; i < arr.length; i++){
+        var randomIndex = Math.floor(Math.random()*arr.length);
+        var tempValOne = arr[i];
+        var tempValTwo = arr[randomIndex];
+        arr[i] = tempValTwo;
+        arr[randomIndex]= tempValOne;
+    }
+    return arr
+}
+function generatePassword(){
+    var finalPassword = [];
+    var possibleCharacters= [];
+    var options = getPasswordOptions();
+    if (options.sChar){
+        var randomsChar = RandomNumber(33, 47);
+        finalPassword.push(randomsChar)
+        for (var i =33; i<=47;i++){
+            possibleCharacters.push(i)
         }
-        var goodPW = password.includes(sChar && nChar && lChar && uChar)
-        console.log(goodPW);
-        
-        //  else ()
-        //  for(var i = 0; i <= passwordLength; i++){
-        //     password = password + passwordValue.charAt(Math.floor(Math.random()*Math.floor(passwordValue.length)));
-        //     }
-
-
-        //     <!--Test code-->
-        // // var test = "4!2Ea@F3#Hd4T$fS5kV%6sTr^7HbgD&BG8sSGF*9v"
-        // // console.log(test.charAt(Math.floor(Math.random()*Math.floor(test.length))));
-
-    
+    }
+    if (options.nChar){
+        var randomnChar = RandomNumber(48, 57);
+        finalPassword.push(randomnChar)
+        for (var i = 48; i<= 57; i++){
+            possibleCharacters.push(i)
+        }
+    }
+    if (options.lChar){
+        var randomlChar = RandomNumber(97, 122);
+        finalPassword.push(randomlChar)
+        for (var i = 97; i<= 122; i++){
+            possibleCharacters.push(i)
+        }
+    }
+    if (options.uChar){
+        var randomuChar = RandomNumber(65, 90);
+        finalPassword.push(randomuChar)
+        for (var i = 65; i<= 90; i++){
+            possibleCharacters.push(i)
+        }
+    }
+    for(var i = finalPassword.length; i < options.length; i++){
+        var randomChar = RandomItemInArray(possibleCharacters);
+        finalPassword.push(randomChar)
+    }
+    for(var i =0; i < finalPassword.length; i++){
+        finalPassword[i] = string.fromCharCode(finalPassword[i]);
+    }
+    return ShuffleArray(finalPassword).join("")
+}
